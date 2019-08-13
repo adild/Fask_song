7f415c8bfb33e25cc098c79827910fe0661212ff
Adil Deshmukh <desadi96@gmail.com>
2019-08-13 15:31:30 +0530

    Procfile added

--- flasksong/__init__.py
+++ flasksong/__init__.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 from flask_mail import Mail
 from flasksong.config import Config
 
 
 db = SQLAlchemy()
-migrate = Migrate(db)
+#migrate = Migrate(db)
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
@@ -23,7 +23,7 @@ def create_app(config_class=Config):
 	app.config.from_object(Config)
 
 	db.init_app(app)
-	migrate.init_app(app)
+	#migrate.init_app(app)
 	bcrypt.init_app(app)
 	login_manager.init_app(app)
 	mail.init_app(app)
